11c11b3a03bd8da55b1187d89b672e95906201f4
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-07-30 14:57:00 +0300

    Created a class with functions that get data from user input

--- password.py
+++ password.py
@@ -1,1 +1,21 @@
 import random
+import string
+
+print(string.ascii_letters)
+
+class Password
+    """
+    Class that generates and stores users passwords
+    """
+
+
+def __init__(self,username,website,password):
+    """
+    Function that dicates data gotten from user input
+    """
+
+    self.username = username
+    self.website = website
+    self.password = password
+
+
