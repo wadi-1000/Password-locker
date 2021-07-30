bfdd454b19a32861e9817de33cc843975b684e26
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-07-30 16:45:36 +0300

    Created a run.py file to run the program

---
+++ run.py
@@ -0,0 +1,7 @@
+def main():
+    print("Welcome to locka the password locker of your choice")
+    name = input()
+    print(input("What is your name?"))
+    print(f"Hi {name}!")
+if __name__ = "__main__"
+    main()
