96ccbacca894cd939e21dddc961a4e1fcccec6d9
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-07-31 12:41:17 +0300

    Adde setUp method to initialize tests

--- password_test.py
+++ password_test.py
@@ -1,5 +1,5 @@
 import unittest
-form password import Password
+from password import Password
 
 class TestPassword(unittest.TestCase):
 
@@ -10,9 +10,99 @@ class TestPassword(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
 
-    def tearDown(self):
-        '''
-        Method that runs tests after the code has run.
-        '''
-        Password.password_list = []
-        
+def setUp(self):
+    '''
+    Method that runs tests before each test case has run.
+    '''
+    self.new_password = Password("wadi","gmail.com","@wadi123") # create contact object
+
+def test_init(self):
+    '''
+    test_init_case to test if the object is being initialized properly
+    '''
+
+    self.assertEqual(self.new_password.username,"wadi")
+    self.assertEqual(self.new_password.website, "gmail.com")
+    self.assertEqual(self.new_password.password,"@wadi123")
+
+
+
+
+
+
+
+
+
+
+
+
+def test_save_password(self):
+    '''
+    test_save_contact test case to test if the password object is saved to the password list
+    '''
+    self.new_password.save_password() #saving the new password
+    self.assertEqual(len(Password.password_list),1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if __name__ ==  '__main__':
+        unittest.main()
