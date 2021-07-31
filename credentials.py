143470f68249948a5b32e8a40f4d9710b313ee7d
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-07-31 16:06:03 +0300

    Created Credentials Class to display user credentials

---
+++ credentials.py
@@ -0,0 +1,20 @@
+class Credentials:
+    '''
+    Class that has user credentials
+    '''
+    credentials = []
+
+
+    def __init__(self,user,website,password)
+    '''
+    Function that holds user credentials
+
+    Args:
+    username: username.
+    website: website.
+    password: password.
+    '''
+
+    self.username = username
+    self.website = website
+    self.password = password
